46845a567d286f1c011c3631bcd26180b9839553
Jonathan White <jonathan.white.jm@gmail.com>
2020-10-11 21:19:19 -0500

    Update .js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,29 @@
 //Lab #3 
 window.onload = function(){
     //Exercise I
+    var currentBoardState = ["", "", "", "", "", "", "", "", ""];
+    var currentTurn = "X";
     var square_boxes = document.getElementById("board").getElementsByTagName("div");     
-    console.log("squares");
 
     for (var i = 0; i < square_boxes.length; i++) {
         square_boxes[i].classList.add("square");
-      }
-}
+        square_boxes[i].addEventListener("click", function(){
+            //switchTurn();
 
+    //Exercise 2
+    //function switchTurn(){
+        if (currentTurn == "X"){
+            this.innerHTML = "X"; 
+            this.classList.add("X");
+            currentTurn = "O";
+            
+        }
+        else{
+            this.innerHTML = "O";
+            this.classList.add("O");
+            currentTurn = "X";
+        }
+    });
+        //}
+}
+}
